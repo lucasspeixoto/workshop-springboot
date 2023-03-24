78f0e0ce607af0132f1aa951cc2456775cb85a74
Lucas Peixoto <lspeixotodev@gmail.com>
2023-03-24 00:05:47 -0300

    User Entity and User resource start Implementation

---
+++ src/main/java/com/lspeixotodev/ecommerce/resourses/UserResource.java
@@ -0,0 +1,23 @@
+package com.lspeixotodev.ecommerce.resourses;
+
+import com.lspeixotodev.ecommerce.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User user = new User(
+                1L,
+                "Liana",
+                "lianacgf@hotmail.com",
+                "19981448980",
+                "liana10");
+        return ResponseEntity.ok().body(user);
+    }
+}
