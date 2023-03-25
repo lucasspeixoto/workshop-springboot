b2f9f51ec966bb2a40924284c37554779962ee80
Lucas Peixoto <lspeixotodev@gmail.com>
2023-03-24 22:52:09 -0300

    Config entity User as Tabele Entity tb_user

--- src/main/java/com/lspeixotodev/ecommerce/entities/User.java
+++ src/main/java/com/lspeixotodev/ecommerce/entities/User.java
@@ -1,5 +1,7 @@
 package com.lspeixotodev.ecommerce.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
@@ -8,16 +10,28 @@ import java.util.Objects;
  * Serializable: Quando queremos uma versão em bites do
  * nosso objeto para trafegar em rede/gravar em arquivo
  */
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
     @Serial
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
     private String email;
+
     private String phone;
+
     private String password;
+
+    public User() {
+    }
 
     public User(Long id, String name, String email, String phone, String password) {
         this.id = id;
